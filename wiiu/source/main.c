9b58083844331c78199a9b2e091a494424c62a7d
Anxietytimmy <71737812+Anxietytimmy@users.noreply.github.com>
2021-10-28 16:03:03 -0700

    Update main.c

--- wiiu/source/main.c
+++ wiiu/source/main.c
@@ -12,14 +12,13 @@
 int
 main(int argc, char **argv)
 {
+
    int a; 
-   int last_tm_sec = -1;
-   OSCalendarTime tm;
    clock_t start = clock()
 
    ;WHBProcInit();
    WHBLogConsoleInit();
-	for (a = 10; a < 9999; a = a + 1) {
+	for (a = 10; a < 99999; a = a + 1) {
 		WHBLogPrintf("value of a: %d\n", a);
 	}
 
@@ -27,19 +26,7 @@ main(int argc, char **argv)
 	double elapsed = (double) (stop - start) * 1000.0 / CLOCKS_PER_SEC;
    WHBLogPrintf("Time elapsed in ms: %f", elapsed);
 
-   while(WHBProcIsRunning()) {
-      OSTicksToCalendarTime(OSGetTime(), &tm);
 
-      if (tm.tm_sec != last_tm_sec) {
-         WHBLogPrintf("%02d/%02d/%04d %02d:%02d:%02d I'm still here.",
-                      tm.tm_mday, tm.tm_mon, tm.tm_year,
-                      tm.tm_hour, tm.tm_min, tm.tm_sec);
-         last_tm_sec = tm.tm_sec;
-       }
-
-      WHBLogConsoleDraw();
-      OSSleepTicks(OSMillisecondsToTicks(100));
-   }
 
    WHBLogPrintf("Exiting... good bye.");
    WHBLogConsoleDraw();
